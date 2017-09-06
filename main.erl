b455b628fd11fa74a2664caf0ef7375c46ddd94b
Dan D'Avella <drdavella@gmail.com>
2017-09-05 22:03:25 -0400

    Practice with lists and binary pattern matching

--- main.erl
+++ main.erl
@@ -2,13 +2,31 @@
 -module(main).
 -export([start/0]).
 
+-define(HELLO, 3).
+
 
 start() ->
-    io:fwrite("Hello, world!\n"),
-    loop(0).
+    Code = [16#f00d, 16#face, 16#b00f, 16#bee7],
+    io:format("Hello ~w, world!\n", [?HELLO]),
+    loop(Code, 0).
 
-loop(10) ->
+process(<<16#f:4, Rest/bits>>) ->
+    io:fwrite("It's an 0xf!"),
+    Op =
+        case Rest of
+            <<16#0:4, _/bits>> -> "a";
+            <<16#a:4, _/bits>> -> "b"
+        end,
+    io:fwrite("op = ~w\n", [Op]);
+process(<<16#b:4, Rest/bits>>) ->
+    io:fwrite("It's a 0xb!");
+process(Unknown) ->
+    io:format("It's something else: ~w\n", [Unknown]).
+
+loop(Code, 10) ->
     io:fwrite("done!\n");
-loop(N) ->
+loop(Code, N) ->
     io:format("~w\n", [N]),
-    loop(N+1).
+    [Head|Tail] = Code,
+    process(<<Head:16>>),
+    loop(Tail ++ [Head], N+1).
