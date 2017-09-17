ac9bf5110623f1f2a878a79a69011329e2118c46
Dan D'Avella <drdavella@gmail.com>
2017-09-16 22:54:17 -0400

    Update cpu to be able to represent 'halt' state

--- cpu.erl
+++ cpu.erl
@@ -1,6 +1,8 @@
 -module(cpu).
 -export([run/1]).
 
+-define(BYTE_MASK, 16#ff).
+
 
 % Some (very) basic logging machinery
 bin_to_int(Bin) when is_binary(Bin) ->
@@ -12,17 +14,33 @@ bin_to_int(Int) when is_integer(Int) ->
 
 run(Code) ->
     State = dict:from_list([
-        {pc, 0}, {a, 0}, {b, 0}, {c, 0}, {d, 0}, {e, 0}, {h, 0}, {l, 0}]),
+        {pc, 0},
+        {sp, 0}, % TODO: where does sp start?
+        {a, 0},
+        {b, 0},
+        {c, 0},
+        {d, 0},
+        {e, 0},
+        {h, 0},
+        {l, 0},
+        {halt, false}]),
     loop(Code, State).
 
 loop(Code, State) ->
     loop(Code, tick(Code, State)).
 
 tick(Code, State) ->
-    Pc = dict:fetch(pc, State),
-    {_, Start} = lists:split(Pc, Code),
-    [Opcode|Rest] = Start,
-    dict:store(pc, decode(<<Opcode>>, Rest, Pc), State).
+    case dict:fetch(halt, State) of
+        true ->
+            io:fwrite("NOP~n"),
+            NewState = State;
+        false ->
+            Pc = dict:fetch(pc, State),
+            {_, Start} = lists:split(Pc, Code),
+            [Opcode|Rest] = Start,
+            NewState = decode(<<Opcode>>, Rest, State)
+    end,
+    NewState.
 
 load_dest(High, LowNibble) ->
     <<Low:4>> = LowNibble,
@@ -37,35 +55,38 @@ do_load(Dest, Source) ->
     io:fwrite("ld ~w->~w~n", [Source, Dest]).
 
 % Disable interrupt
-decode(<<16#f3>>, _, Pc) ->
+decode(<<16#f3>>, _, State) ->
     io:fwrite("disable interrupt~n"),
-    Pc + 1;
+    dict:update_counter(pc, 1, State);
 % HALT instruction
-decode(<<16#76>>, _, Pc) ->
+decode(<<16#76>>, _, State) ->
     io:fwrite("HALT"),
-    % TODO: This is not the correct behavior, but is just a workaround for now
-    Pc + 1;
+    % TODO: update this once interrupts actually work
+    % NewState = dict:store(halt, true, State),
+    % dict:store(pc, dict:fetch(pc, NewState) + 1, NewState);
+    dict:update_counter(pc, 1, State);
 % ADD operations
-decode(<<16#8:4, LowNibble/bits>>, _, Pc) ->
+decode(<<16#8:4, LowNibble/bits>>, _, State) ->
     io:fwrite("op2=~w~n", [op2(LowNibble)]),
-    Pc + 1;
+    dict:update_counter(pc, 1, State);
 % SUB operations
-decode(<<16#9:4, LowNibble/bits>>, _, Pc) ->
-    Pc + 1;
+decode(<<16#9:4, LowNibble/bits>>, _, State) ->
+    dict:update_counter(pc, 1, State);
 % Bitwise AND operations
-decode(<<16#A:4, LowNibble/bits>>, _, Pc) ->
-    Pc + 1;
+decode(<<16#A:4, LowNibble/bits>>, _, State) ->
+    dict:update_counter(pc, 1, State);
 % Bitwise OR operations
-decode(<<16#B:4, LowNibble/bits>>, _, Pc) ->
-    Pc + 1;
+decode(<<16#B:4, LowNibble/bits>>, _, State) ->
+    dict:update_counter(pc, 1, State);
 % Load operations
-decode(<<H:4, LowNibble/bits>>, _, Pc) when H >= 4, H =< 7 ->
+decode(<<H:4, LowNibble/bits>>, _, State) when H >= 4, H =< 7 ->
     <<NibbleVal:4/integer>> = LowNibble,
     io:fwrite("LD: 0x~w~.16B: ", [H, NibbleVal]),
     do_load(load_dest(H, LowNibble), op2(LowNibble)),
-    Pc + 1;
+    dict:update_counter(pc, 1, State);
 % Unrecognized instruction (error condition, used for development)
-decode(Unknown, _, Pc) ->
+decode(Unknown, _, State) ->
+    Pc = dict:fetch(pc, State),
     io:format("unknown instruction (pc=~w): 0x~.16B~n",
               [Pc, bin_to_int(Unknown)]),
-    Pc + 1.
+    dict:update_counter(pc, 1, State).
