e6793c6a93ce89e91e823b05065b16c5857a20f7
Dan D'Avella <drdavella@gmail.com>
2017-09-29 17:01:45 -0400

    Add skeleton of functionality to load memory

--- cpu.erl
+++ cpu.erl
@@ -58,10 +58,17 @@ get_hl(State) ->
 read_mem(State, Addr) ->
     array:get(Addr, dict:fetch(mem, State)).
 
+write_mem(State, Addr, Value) ->
+    State.
+
+
 do_load(State, Dest, Source) when Source == hl ->
     Address = get_hl(State),
     io:fwrite("read memory at addr=~w~n", [Address]),
     dict:store(Dest, read_mem(State, Address), State);
+do_load(State, Dest, Source) when Dest == hl ->
+    io:fwrite("load memory from ~w!~n", [Source]),
+    write_mem(State, get_hl(State), 0);
 do_load(State, Dest, Source) ->
     io:fwrite("ld ~w->~w~n", [Source, Dest]),
     dict:store(Dest, dict:fetch(Source, State), State).
